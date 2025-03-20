087574847a502307cbb2b05ca0eda24e0063a81c
jnow424 <now4940@gmail.com>
2025-03-20 16:44:21 +0900

    2025.03.20
    이메일 보내기

---
+++ Project/src/main/java/com/test/jajva/memo/Add.java
@@ -0,0 +1,5 @@
+package com.test.jajva.memo;
+
+public class Add {
+
+}
---
+++ Project/src/main/java/com/test/java/user/model/UserDTO.java
@@ -0,0 +1,30 @@
+package com.test.java.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String pic;
+	private String intro;
+	private String regdate;
+	private String ing;
+}
+
+
+
+
+
+
+
+
+
+
+
